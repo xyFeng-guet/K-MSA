73a4a22fd8189c7fdb4c9361164a9e42a1344433
xyFeng-guet <515839491@qq.com>
2024-04-27 18:11:42 +0800

    preprocess text

--- pretrainedModel/preprocess_language.py
+++ pretrainedModel/preprocess_language.py
@@ -1,6 +1,5 @@
 import requests
 import random
-import json
 import hashlib
 import pickle
 
@@ -15,36 +14,41 @@ class BaiDuFanyi:
         self.salt = random.randint(32768, 65536)
         self.header = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    def BdTrans(self, text):
-        sign = self.appid + text + str(self.salt) + self.secretKey
-        md = hashlib.md5()
-        md.update(sign.encode(encoding='utf-8'))
-        sign = md.hexdigest()
-        data = {
-            "appid": self.appid,
-            "q": text,
-            "from": self.fromLang,
-            "to": self.toLang,
-            "salt": self.salt,
-            "sign": sign
-        }
-        response = requests.post(self.url, params= data, headers= self.header)  # 发送post请求
-        text = response.json()  # 返回的为json格式用json接收数据
-        print(text)
-        results = text['trans_result'][0]['dst']
-        return results
+    def BdTrans(self, textDataset):
+        translateText = []
+        for text in textDataset:
+            sign = self.appid + text + str(self.salt) + self.secretKey
+            md = hashlib.md5()
+            md.update(sign.encode(encoding='utf-8'))
+            sign = md.hexdigest()
+            data = {
+                "appid": self.appid,
+                "q": text,
+                "from": self.fromLang,
+                "to": self.toLang,
+                "salt": self.salt,
+                "sign": sign
+            }
+            response = requests.post(self.url, params=data, headers=self.header)    # 发送post请求
+            text = response.json()  # 返回的为json格式用json接收数据
+            translateText.append(text['trans_result'][0]['dst'])
+
+        return translateText
 
 
-def ReadData():
-    with open('/opt/data/private/Project/Datasets/MSA_Datasets/SIMS/Processed/unaligned_39.pkl', 'rb') as f:
-        data = pickle.load(f)
+def ReadSaveData(do):
+    if do == 'read':
+        with open('/opt/data/private/Project/Datasets/MSA_Datasets/SIMS/Processed/unaligned_39.pkl', 'rb') as f:
+            data = pickle.load(f)
 
-    rawText = []
-    for types in ['train', 'valid', 'test']:
-        for text in data[types]['raw_text']:
-            rawText.append(text)
+        rawText = []
+        for types in ['train', 'valid', 'test']:
+            for text in data[types]['raw_text']:
+                rawText.append(text)
 
-    return rawText
+        return rawText
+    else:
+        pass
 
 
 if __name__ == '__main__':
@@ -52,7 +56,8 @@ if __name__ == '__main__':
     appSecret = 'q55okNouXDa0e8_qu95V'      # 公钥
     BaiduTranslate_test = BaiDuFanyi(appKey, appSecret)
 
-    Text_data = ReadData()
+    Text_data = ReadSaveData('read')
 
     Results = BaiduTranslate_test.BdTrans(Text_data)      # 要翻译的词组
-    print(Results)
+
+    ReadSaveData('save')
